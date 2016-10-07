3b052324c81767f25c1176c9d886a98a3f5d0a16
Fabiana Ferreira Fonseca <fabiana.ferreira@poli.ufrj.br>
2016-10-06 21:58:46 -0300

    Colors were changed

--- design_variables.js
+++ design_variables.js
@@ -1,4 +1,9 @@
-var femaleAliveColor = "#ff16cc";
+/*var femaleAliveColor = "#ff16cc";
 var femaleDeadColor = "#ffccff";
 var maleAliveColor = "#3399ff";
-var maleDeadColor = "#b3e6ff";
+var maleDeadColor = "#b3e6ff";*/
+
+var femaleColor = "#f980bf";
+var femaleSpouseColor = "#edcce0";
+var maleColor = "#00c0f4";
+var maleSpouseColor = "#a4c9ea";
